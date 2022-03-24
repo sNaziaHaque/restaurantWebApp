240bbf08d72344f5fac2e6cca6c488014fd94a54
sNaziaHaque <nazia.haque.snh@gmail.com>
2022-03-24 00:33:05 -0400

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import Home from "./pages/Home";
+import AddEdit from "./pages/AddEdit";
+import View from "./pages/View";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer position="top-center" />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/add" element={<AddEdit />} />
+          <Route exact path="/edit/:id" element={<AddEdit />} />
+          <Route exact path="/view/:id" element={<View />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
