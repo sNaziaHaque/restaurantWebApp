c5215158ae9af64b2101f84103c5e40966c0dfb2
sNaziaHaque <nazia.haque.snh@gmail.com>
2022-04-04 18:19:51 -0400

    add restaurant view page

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import fireDb from "../firebase";
+import { useNavigate, useParams } from "react-router-dom";
+import Carousel from "react-bootstrap/Carousel";
+
+import "./View.css";
 
 function View() {
-  return <div>View</div>;
+  const [restaurant, setRestaurant] = useState({});
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fireDb
+      .child(`restaurants/${id}`)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          setRestaurant({ ...snapshot.val() });
+        } else {
+          setRestaurant({});
+        }
+      });
+  }, [id]);
+
+  return (
+    <div>
+      <div>
+        <Carousel className="restaurant-carousal">
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="../../images/1.jpg"
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="../../images/2.jpg"
+              alt="Second slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="../../images/3.jpg"
+              alt="Third slide"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+      <h3>{restaurant.name}</h3>
+      <div className="row">
+        <div className="col-md-3 offset-md-1 shadow p-3 mb-5 bg-white rounded">
+          Restaurant Info
+          <hr />
+          <div className="info-text">
+            <p>Name - {restaurant.name}</p>
+            <p>Address - {restaurant.address}</p>
+            <p>Rating - {restaurant.rating}</p>
+          </div>
+        </div>
+        <div className="col-md-1"></div>
+        <div className="col-md-6 shadow p-3 mb-5 bg-white rounded">
+          Menu
+          <hr />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default View;
